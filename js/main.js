1ffc753daae89ecde5456b544cfb043ef3111f45
Lailla Galeno <laillagalenoitz@gmail.com>
2022-05-20 15:11:22 -0300

    Estilizando Menu com css e jquery

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+
+     window.location.assign("#nav1");
+     var url = "#nav1";
+
+     $('.menu-itens li a').each(function(){
+          var href = $(this).attr('href');
+               
+          if (url === href){
+               $(this).addClass("active"); 
+          }
+        });
+
+     //destaque no menu
+     $(function() {                       
+          $(".menu-itens li a").click(function() {  
+               $(".menu-itens li a").removeClass("active");
+               $(this).addClass("active");
+               var href = $(this).attr('href');
+          });
+          
+        });
+ });
